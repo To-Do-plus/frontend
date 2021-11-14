52472b97e345d70630ec479796fb0784db09067d
Chris Rarig <cwrarig20@gmail.com>
2021-11-13 20:58:19 -0600

    added crud code

--- src/Components/AddTDItem.js
+++ src/Components/AddTDItem.js
@@ -7,27 +7,27 @@ import Form from 'react-bootstrap/Form';
 class AddTDItem extends React.Component {
 
 
-  handleChange = (e) => {
-    e.preventDefault();
-    const summary = e.target.summary;
-    const location = e.target.location;
-    const description = e.target.description;
-    const startTime = e.target.startTime;
-    const endTime = e.target.endTime;
-  }
+  // handleChange = (e) => {
+  //   e.preventDefault();
+  //   const summary = e.target.summary.value;
+  //   const location = e.target.location.value;
+  //   const description = e.target.description.value;
+  //   const startTime = e.target.startTime.value;
+  //   const endTime = e.target.endTime.value;
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault();
     let newTask = {
-      summary: (e.target.summary),
-      location: (e.target.location),
-      description: (e.target.description),
+      summary: (e.target.summary.value),
+      location: (e.target.location.value),
+      description: (e.target.description.value),
       start: {
-        dateTime: (e.target.startTime),
+        dateTime: (e.target.startTime.value),
         timeZone: this.props.timeZone,
       },
       end: {
-        dateTime: (e.target.endTime),
+        dateTime: (e.target.endTime.value),
         timeZone: this.props.timeZone,
       },
     }
@@ -35,23 +35,23 @@ class AddTDItem extends React.Component {
   }
 
   render() {
-
+    // onChange={this.handleChange}
     return (
       <Form data-testid="add-form" onSubmit={this.handleSubmit}>
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '20rem' }}>
           <Card.Header>What's On Your List?</Card.Header>
           <Card.Body>
             <Form.Group>
               <Form.Label>Task Summary</Form.Label>
-              <Form.Control type="text" placeholder="What do you need to do?" summary="summary" onChange={this.handleChange} />
+              <Form.Control type="text" placeholder="What do you need to do?" id="summary"  />
               <Form.Label>Task Location</Form.Label>
-              <Form.Control type="text" placeholder="Where does this need to happen?" location="location" onChange={this.handleChange} />
+              <Form.Control type="text" placeholder="Where does this need to happen?" id="location"  />
               <Form.Label>Task Description</Form.Label>
-              <Form.Control type="text" placeholder="Any details to remember?" description="description" onChange={this.handleChange} />
+              <Form.Control type="text" placeholder="Any details to remember?" id="description"  />
               <Form.Label>Start Time</Form.Label>
-              <Form.Control type="text" placeholder="When do you want to start?" startTime="startTime" onChange={this.handleChange} />
+              <Form.Control type="text" placeholder="When do you want to start?" id="startTime"  />
               <Form.Label>End Time</Form.Label>
-              <Form.Control type="text" placeholder="How long will this task take?" endTime="endTime" onChange={this.handleChange} />
+              <Form.Control type="text" placeholder="How long will this task take?" id="endTime"  />
             </Form.Group>
             <Button variant="primary" type="submit">Add To The List!</Button>
           </Card.Body>
