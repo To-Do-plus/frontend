b7e63404d39d1ad4c1853f51b05b4529b43c3a74
Jaimierl <jaimieloney@gmail.com>
2021-11-14 19:44:39 -0500

    About me bio

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -26,8 +26,7 @@ class AboutMe extends React.Component {
               <Card.Body>
                 <Card.Title>Jae Loney</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Jae is a Software Developer with experience in Project Management and Accounts Management who loves learning, solving problems, and organizing teams. Jae enjoys working with Javascript in both front-end and back-end applications and hopes to use their skills to bring websites to life with people who are working to change the world for the better. In the unlikely event of having free time, Jae loves exploring New York City (and anywhere else they can get to!), writing, and performing Sketch Comedy.
                 </Card.Text>
               </Card.Body>
             </Card>
