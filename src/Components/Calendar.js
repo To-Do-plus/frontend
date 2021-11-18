5a7c3910434a29717fbf93b3ba72cf3e74c092f3
Anthony Turinski <71283758+Terransky@users.noreply.github.com>
2021-11-17 17:57:53 -0800

    Update Calendar.js

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -137,13 +137,13 @@ function renderEventContent(eventInfo) {
   )
 }
 
-function renderSidebarEvent(event) {
-  return (
-    <li key={event.id}>
-      <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
-      <i>{event.title}</i>
-    </li>
-  )
-}
+// function renderSidebarEvent(event) {
+//   return (
+//     <li key={event.id}>
+//       <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
+//       <i>{event.title}</i>
+//     </li>
+//   )
+// }
 
 export default Calendar;
